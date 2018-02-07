bb5c9165b60a7da8bb5acfff62b4f1db18b6641b
David Sawyer <davidsawyer10@gmail.com>
2018-02-07 00:45:27 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Augusta Health Resources</title>
+</head>
+<style>
+    body {
+        background: #0d2540;
+        background: -moz-linear-gradient(125deg, #0d2540 0%, #183254 100%);
+        background: -webkit-linear-gradient(125deg, #0d2540 0%, #183254 100%);
+        background: linear-gradient(325deg, #0d2540 0%, #183254 100%);
+        background-repeat: no-repeat;
+        color: white;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
+            "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+        height: 100vh;
+        margin: 0;
+    }
+
+    h1 {
+        margin-top: 0;
+        padding-top: 200px;
+    }
+
+    h1, h2 {
+        text-align: center;
+        font-weight: 600;
+    }
+</style>
+<body>
+    <h1>Augusta Health Resources</h1>
+    <h2>Coming soon...</h2>
+</body>
+</html>
